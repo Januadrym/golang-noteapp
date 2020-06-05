50680e10315073f602ab1a87255454f3eef00128
januadrym <mquandlvn@gmail.com>
2020-06-05 11:39:28 +0700

    change-to-old-stage

---
+++ location.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/spf13/cobra"
+
+func directory() *cobra.Command {
+	return &cobra.Command{
+		Use:   "dir",
+		Short: "Pick directory for saving note, otherwise at default",
+	}
+}
